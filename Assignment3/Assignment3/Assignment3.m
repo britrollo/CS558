1bf31514969b83231b81ae8c135165f7b0b6e199
btrollo <btrollo@stevens.edu>
2019-10-27 00:13:32 -0400

    debugging time

--- Assignment3/Assignment3/Assignment3.m
+++ Assignment3/Assignment3/Assignment3.m
@@ -2,7 +2,8 @@ function main ()
     %k-means
     k = 10;
     img = imread("white-tower.png");
-    
+    result = k_means(k, img)
+    imshow(result);
     %SLIC 
     
 end
@@ -65,9 +66,35 @@ function result = closest_cluster(img, px, py, c)
     return;
 end
 
+function result = check_ci(og, newc, k)
+    % Parameters
+        % og -> original cluster centers
+        % newc -> new cluster centers
+    % Return
+        % result ->
+            % true -> original and new cluster centers are the same
+            % false -> original and new cluster centers are NOT the same
+    found = zeros(1,k);
+    for i=1:k
+        for j=1:k
+            if (og(i,1) == newc(j,1)) && (og(j,2) == newc(j,2))
+                found(i) = 1;
+                break;
+            end
+        end
+    end
+    if sum(found) == k
+        result = true;
+        return;
+    else
+        result = false;
+        return;
+    end
+end
+
 function result = k_means(k, img)
     %Get size of image
-    [X, Y] = size(img);
+    [X, Y, col] = size(img);
     
 %%% STEP 1: Randomly initialize the cluster centers, c1, ..., ck
 
@@ -88,27 +115,93 @@ function result = k_means(k, img)
         cluster_centers(c, 2) = py;
     end
     
-%%% STEP 2: Given cluster centers, determine points in each cluster
-%%%         For each point p, find the closest ci. Put p into cluster i
+    flag = false;
+    while flag ~= true
+    %%% STEP 2: Given cluster centers, determine points in each cluster
+    %%%         For each point p, find the closest ci. Put p into cluster i
+
+        % initialize matrix for holding points per cluster
+        % X*Y points, [x, y, ci]
+        clustered = zeros(X*Y, 3);
+        cnt = 1;
+        for x=1:X
+            for y=1:Y
+                % Find closest cluster center
+                ci = closest_cluster(img, x, y, cluster_centers);
+                clustered(cnt, 3) = ci;
+                clustered(cnt, 1) = x;
+                clustered(cnt, 2) = y;
+                cnt = cnt + 1;
+            end
+        end
+
+    %%% STEP 3: Given points in each cluster, solve for ci
+    %%%         Set ci to be the mean of points in cluster i
+        new_cluster_centers = zeros(k, 2);
+        for ci=1:k
+            n = 0;
+            sumx = 0;
+            sumy = 0;
+            for i=1:(X*Y)
+                if clustered(i, 3) == ci
+                    n = n + 1;
+                    sumx = sumx + clustered(i, 1);
+                    sumy = sumy + clustered(i, 2);
+                end
+            end
+            new_cluster_centers(ci, 1) = floor(sumx/n);
+            new_cluster_centers(ci, 2) = floor(sumy/n);
+        end
+
+    %%% STEP 4: If ci have changed, repeat STEP 2
+        flag = check_ci(cluster_centers, new_cluster_centers, k);
+    end
     
-    % initialize matrix for holding points per cluster
-    % X*Y points, [x, y, ci]
-    clustered = zeros(X*Y, 3);
-    cnt = 1;
-    for x=1:X
-        for y=1:Y
-            % Find closest cluster center
-            ci = closest_cluster(img, x, y, cluster_centers);
-            clustered(cnt, 3) = ci;
-            clustered(cnt, 1) = x;
-            clustered(cnt, 2) = y;
+    % Create final image - Represent each cluster with the average RGB
+    % value of its members
+    
+    % Initialize resulting image
+    c_img = zeros(X, Y, 3);
+    % Calculate average RGB value of cluster
+    c_avg_rgb = zeros(k, 3);
+    for ci=1:k
+        n = 0;
+        sumr = 0;
+        sumg = 0;
+        sumb = 0;
+        for i=1:(X*Y)
+            if clustered(i, 3) == ci
+                n = n + 1;
+                % pixel coordinates from cluster ci
+                px = clustered(i,1);
+                py = clustered(i,2);
+                % RGB value from original image
+                rgb = img(px, py, :);
+                sumr = sumr + rgb(1);
+                sumg = sumg + rgb(2);
+                sumb = sumb + rgb(3);
+            end
+            c_avg_rgb(ci, 1) = floor(sumr/n);
+            c_avg_rgb(ci, 2) = floor(sumg/n);
+            c_avg_rgb(ci, 3) = floor(sumb/n);
         end
     end
-
-%%% STEP 3: Given points in each cluster, solve for ci
-%%%         Set ci to be the mean of points in cluster i
     
-
-%%% STEP 4: If ci have changed, repeat STEP 2
+    % Set new image coordinates to RGB average of each cluster 
+    for ci=1:k
+        for i=1:(X*Y)
+            if clustered(i, 3) == ci
+                x = clustered(i, 1);
+                y = clustered(i, 2);
+                r = c_avg_rgb(ci, 1);
+                g = c_avg_rgb(ci, 2);
+                b = c_avg_rgb(ci, 3);
+                c_img(x, y, 1) = r;
+                c_img(x, y, 2) = g;
+                c_img(x, y, 3) = b;
+            end
+        end
+    end
     
+    result = c_img;  
 end 
