2018e3f8091ffa210d3d7d077fee2c7218d98582
britrollo <britrollo@gmail.com>
2019-11-14 23:11:48 -0500

    Problem 1 first draft done

--- Assignment4/cs558s18_hw4/Assignment4.m
+++ Assignment4/cs558s18_hw4/Assignment4.m
@@ -6,6 +6,19 @@ function main()
     result = classifyIm(bins, directory);
 end
 
+function result = twentyfourdistance(test, train)
+    closest_dist = inf;
+    idx = 0;
+    for i=1:length(train)
+        test_dist = sqrt((sum((test.r-train(i).r).^2))+(sum((test.g-train(i).g).^2))+(sum((test.b-train(i).b).^2)));
+        if test_dist < closest_dist
+            idx = i;
+            closest_dist = test_dist;
+        end
+    end
+    result = idx;
+end
+
 % PROBLEM 1: Image Classification
 function result = classifyIm(bins, directory)
 % Step1: Get images from ImClass directory
@@ -39,6 +52,34 @@ function result = classifyIm(bins, directory)
             disp("Voting Error: " + train_images(ii).name);
         end
     end
+    
+    % Step 5: compute 3 histogram representation for test images
+    test_hists(1:length(test_images)) = struct('r', zeros(1, bins), 'g', zeros(1, bins), 'b', zeros(1, bins));
+    correct = 0;
+    for ii=1:length(test_images)
+        img = imread(strcat(test_images(ii).folder, "/", test_images(ii).name));
+        img = double(img);
+        [X, Y, ~] = size(img);
+        for i=1:X
+            for j=1:Y
+                % Step3: Each pixel votes in each histogram
+                div = ceil((255+1)/bins);   % (255+1) to cover range from 0 to 255
+                rgb = img(i,j,:);
+                rv = rgb(1)+1;              % +1 to adjust for 0
+                test_hists(ii).r(ceil(rv/div)) = test_hists(ii).r(ceil(rv/div)) + 1;
+                gv = rgb(2)+1;
+                test_hists(ii).g(ceil(gv/div)) = test_hists(ii).g(ceil(gv/div)) + 1;
+                bv = rgb(3)+1;
+                test_hists(ii).b(ceil(bv/div)) = test_hists(ii).b(ceil(bv/div)) + 1;
+            end
+        end
+        closest = twentyfourdistance(test_hists(ii), train_hists);
+        disp("Test image " + string(ii) + " of class " + string(ceil(ii/4)) + " has been assigned to class " + string(ceil(closest/4)) + ".");
+        if ceil(ii/4) == ceil(closest/4)
+            correct = correct + 1;
+        end
+    end
+    disp("Accuracy: " + string(correct/length(test_images)));
     
 % Classify each image into one of three classes: coast, forest, or "insidecity"
 end
